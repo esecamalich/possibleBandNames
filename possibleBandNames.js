a207fc74aa4e16d8716c69aa747d0bcea4e4b8b4
Sergio Camalich <sergio@esecamalich.com>
2022-11-24 18:19:12 -0600

    Turn two original band names and create an array of each.
    Define two original band names.
    Turn both names into an array.

---
+++ possibleBandNames.js
@@ -0,0 +1,16 @@
+// Define two original band names
+let firstBandName = 'Goo Goo Dolls';
+let secondBandName = 'Foo Fighters';
+console.log('The chosen band names are ' + firstBandName + ' and ' + secondBandName);
+
+// Turn both names into an array
+let firstBandArray = firstBandName.split(' ');
+console.log(firstBandArray);
+
+let secondBandArray = secondBandName.split(' ');
+console.log(secondBandArray);
+
+
+
+//let possibleBandNames = secondBandArray.map(secondBandArray => firstBandArray.map(firstBandArray => firstBandArray + ' ' + secondBandArray));
+//console.log(possibleBandNames);
